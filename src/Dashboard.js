db2b7b9a887340889645837aab3f343fdce94a7b
natsumai <summerete0809natsu@yahoo.co.jp>
2022-12-09 06:26:28 +0000

    Added Dashboard.js

---
+++ src/Dashboard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Dashboard = ({ signOut, user }) => {
+  const test = () => {
+    alert(2)
+  }
+ 
+
+  return (
+    <>
+    <div>dashboard 3</div>
+    
+    <Link to={'/'}>post</Link>
+    </>
+  )
+}
+
+
+export default Dashboard;
+
+
+Feedback
+Looking for language selection? Find it in the new 
+Unified Settings
+© 2022, Amazon Web Ser
