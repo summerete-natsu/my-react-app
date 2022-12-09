c5eaf8f5069a61e91b8c4bf03544a4fbaf98ccf2
natsumai <summerete0809natsu@yahoo.co.jp>
2022-12-09 18:34:43 +0900

    dd

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,26 +1,14 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Dashboard = ({ signOut, user }) => {
-  const test = () => {
-    alert(2)
-  }
- 
-
   return (
     <>
-    <div>dashboard 3</div>
-    
-    <Link to={'/'}>post</Link>
-    </>
-  )
-}
+      <div>dashboard 3</div>
 
+      <Link to={"/"}>post</Link>
+    </>
+  );
+};
 
 export default Dashboard;
-
-
-Feedback
-Looking for language selection? Find it in the new 
-Unified Settings
-© 2022, Amazon Web Ser
