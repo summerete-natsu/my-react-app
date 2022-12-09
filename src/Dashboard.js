98007b84a215f21e1cf42d44800de2f6f54381a7
natsumai <summerete0809natsu@yahoo.co.jp>
2022-12-09 18:54:14 +0900

    dd

---
+++ src/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Dashboard = ({ signOut, user }) => {
+ 
+  return (
+    <>
+    <div>dashboard 3</div>
+    
+    <Link to={'/'}>post</Link>
+    </>
+  )
+}
+
+
+export default Dashboard;
---
+++ src/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import About from "./About";
+import Dashboard from "./Dashboard";
+import reportWebVitals from "./reportWebVitals";
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
+
+import { Route, Routes, BrowserRouter, Link, Outlet } from "react-router-dom";
+
+Amplify.configure(awsExports);
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
