c508dfa3bffe678d3ec144a77e5e2dea153351ae
natsumai <summerete0809natsu@yahoo.co.jp>
2024-05-05 16:18:57 +0900

    commited

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -7,54 +8,97 @@ const Dashboard = ({ signOut, user }) => {
     <div className="containers-dash">
       </div>
       <hr className="hr" />
-      <h2>お土産</h2>
+      <h2>お土産‐一般観光向け</h2>
       <div className="h-area">
         <div className="s1 h-area-su"></div>
         <div className="s2 h-area-su"></div>
         <div className="s3 h-area-su"></div>
         <div className="s4 h-area-su"></div>
         <div className="info">
-            <div>ベーキング</div>
+            <div>cookies  </div>
           </div>
       </div>
-
-      <div className="containers-dash">
-      </div>
-      <hr className="hr" />
-      <h2>お土産</h2>
       <div className="h-area">
         <div className="s1 h-area-su"></div>
         <div className="s2 h-area-su"></div>
         <div className="s3 h-area-su"></div>
         <div className="s4 h-area-su"></div>
         <div className="info">
-            <div>ベーキング</div>
+            <div>化粧品 </div>
           </div>
       </div>
-
-      <div className="containers-dash">
-      </div>
-      <hr className="hr" />
-      <h2>お土産</h2>
       <div className="h-area">
         <div className="s1 h-area-su"></div>
         <div className="s2 h-area-su"></div>
         <div className="s3 h-area-su"></div>
         <div className="s4 h-area-su"></div>
         <div className="info">
-            <div>ベーキング</div>
+            <div>電気ガジェット  </div>
+          </div>
+      </div>
+      <div className="h-area">
+        <div className="s1 h-area-su"></div>
+        <div className="s2 h-area-su"></div>
+        <div className="s3 h-area-su"></div>
+        <div className="s4 h-area-su"></div>
+        <div className="info">
+            <div>アクセサリー  </div>
           </div>
       </div>
 
       <div className="containers-dash">
       </div>
       <hr className="hr" />
-      <h2>お土産</h2>
+      <h2>お土産：小物雑貨３‐５歳‐</h2>
       <div className="h-area">
-        <div className="s1 h-area-su"></div>
-        <div className="s2 h-area-su"></div>
-        <div className="s3 h-area-su"></div>
-        <div className="s4 h-area-su"></div>
+        <div className="h1 h-area-fu"></div>
+        <div className="h2 h-area-fu"></div>
+        <div className="h3 h-area-fu"></div>
+        <div className="h4 h-area-fu"></div>
+        <div className="h5 h-area-fu"></div>
+        <div className="info">
+            <div>キャラクター</div>
+            <div>BadBazmaru</div>
+            <div>My melody</div>
+            <div>Cinnamonrol</div>
+            <div>Pompompudding</div>
+            <div>Kuromig</div>
+          </div>
+      </div>
+
+      <div className="containers-dash">
+      </div>
+      <hr className="hr" />
+      <h2>お土産：文房具　6－18歳</h2>
+      <div className="h-area">
+        <div className="sss1 h-area-sss"></div>
+        <div className="sss2 h-area-sss"></div>
+        <div className="sss3 h-area-sss"></div>
+        <div className="sss4 h-area-sss"></div>
+        <div className="sss5 h-area-sss"></div>
+        <div className="info">
+            <div>すみっこぐらし</div>
+            <div>しろくま</div>
+            <div>ペンギン</div>
+            <div>とんかつやねこ</div>
+            <div>とかげ</div>
+          <div></div>
+          </div>
+      </div>
+
+      <div className="containers-dash">
+      </div>
+      <hr className="hr" />
+      <h2>お土産：スペシャル版　ウマ娘🏇漫画</h2>
+      <div className="h-area">
+        <div className="uma1 h-area-uma"></div>
+        <div className="uma2 h-area-uma"></div>
+        <div className="uma3 h-area-su"></div>
+        <div className="uma4 h-area-su"></div>
+        <div className="uma5 h-area-su"></div>
+        <div className="uma6 h-area-su"></div>
+        <div className="uma7 h-area-su"></div>
+        <div className="uma8 h-area-su"></div>
         <div className="info">
             <div>ベーキング</div>
           </div>
